cf0ab281808dcf52ba85186de3acfd1538006e98
username <jiminkim0131@gmail.com>
2024-07-28 16:28:45 +0900

    Style: 사이드바 UI 수정

--- lib/widgets/main_sidebar.dart
+++ lib/widgets/main_sidebar.dart
@@ -4,27 +4,49 @@ class MainSidebar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 60, // 사이드바의 너비를 설정
+      width: 100, // 사이드바의 너비를 설정
       color: Colors.grey[200], // 배경색 설정
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start, // 위쪽 정렬
         children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.home), // home (채팅 메인)
-            onPressed: () {},
+          Column(
+            children: [
+              IconButton(
+                icon: Icon(Icons.home), // home (채팅 메인)
+                onPressed: () {},
+              ),
+              Text('홈')
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.dashboard), // dashboard (대시보드)
-            onPressed: () {},
+          SizedBox(height: 20), // Column 사이의 간격
+          Column(
+            children: [
+              IconButton(
+                icon: Icon(Icons.dashboard), // dashboard (대시보드)
+                onPressed: () {},
+              ),
+              Text('대시보드')
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.settings), // 설정 
-            onPressed: () {},
+          SizedBox(height: 20), // Column 사이의 간격
+          Column(
+            children: [
+              IconButton(
+                icon: Icon(Icons.settings), // 설정 
+                onPressed: () {},
+              ),
+              Text('설정')
+            ],
           ),
           Spacer(),
-          IconButton(
-            icon: Icon(Icons.account_circle), // 마이페이지
-            onPressed: () {},
+          Column(
+            children: [
+              IconButton(
+                icon: Icon(Icons.account_circle), // 마이페이지
+                onPressed: () {},
+              ),
+              Text('마이페이지')
+            ],
           ),
         ],
       ),
