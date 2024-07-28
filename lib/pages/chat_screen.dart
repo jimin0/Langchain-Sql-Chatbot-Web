dbbba6b942af7339ba9f61a98ab6f878be0388e9
username <jiminkim0131@gmail.com>
2024-07-28 21:00:37 +0900

    Design: 배경색 및 border line 추가

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -14,6 +14,7 @@ class ChatScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Chat Screen'),
       ),
+      backgroundColor: Colors.white,
       body: Row(
         children: <Widget>[
           MainSidebar(), // 사이드바
--- lib/widgets/main_sidebar.dart
+++ lib/widgets/main_sidebar.dart
@@ -5,7 +5,14 @@ class MainSidebar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 100, // 사이드바의 너비를 설정
-      color: Colors.grey[200], // 배경색 설정
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(
+            color: Color.fromARGB(173, 191, 179, 179), // 사이드바의 오른쪽 테두리 색상
+            width: 1.0, // 사이드바의 오른쪽 테두리 두께
+          ),
+        ),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start, // 위쪽 정렬
         children: <Widget>[
