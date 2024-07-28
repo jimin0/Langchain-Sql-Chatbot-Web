545e94f93fcbf600e84ef577bb1d24389df0b74e
username <jiminkim0131@gmail.com>
2024-07-28 16:24:34 +0900

    Feat: 사이드바 추가

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -3,6 +3,7 @@ import '../widgets/main_chat_header.dart';
 import '../widgets/main_chat_recent_view.dart';
 import '../widgets/main_chat_reco_buttons.dart';
 import '../widgets/main_chat_search_bar.dart';
+import '../widgets/main_sidebar.dart';
 
 // 메인화면이자 채팅화면
 
@@ -13,23 +14,30 @@ class ChatScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Chat Screen'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            MainChatHeader(), // 채팅화면 헤더
-            SizedBox(height: 100), 
-            MainChatRecoButtons(), // 추천 질문 버튼
-            SizedBox(height: 20),
-            MainChatSearchBar(),  // 검색바
-            SizedBox(height: 80),
-            Expanded(child: MainChatRecentView()), // 최근 조회 콘텐츠(대화) 목록
-
-
-            // 다른 UI 요소 추가예정
-          ],
-        ),
+      body: Row(
+        children: <Widget>[
+          MainSidebar(), // 사이드바
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  MainChatHeader(), // 채팅화면 헤더
+                  SizedBox(height: 100), 
+                  MainChatRecoButtons(), // 추천 질문 버튼
+                  SizedBox(height: 20),
+                  MainChatSearchBar(),  // 검색바
+                  SizedBox(height: 80),
+                  Expanded(child: MainChatRecentView()), // 최근 조회 콘텐츠(대화) 목록
+            
+            
+                  // 다른 UI 요소 추가예정
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
