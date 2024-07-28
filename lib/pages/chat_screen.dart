8666e311bbbce41a9c462d207291ea1b1bc07e5c
username <jiminkim0131@gmail.com>
2024-07-28 16:05:17 +0900

    Feat: ChatScreen에 여러 UI 요소 추가

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_chat_header.dart';
+import '../widgets/main_chat_recent_view.dart';
+import '../widgets/main_chat_reco_buttons.dart';
+import '../widgets/main_chat_search_bar.dart';
 
-// 메인화면이자 채팅 화면 
+// 메인화면이자 채팅화면
 
 class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('채팅'),
+        title: Text('Chat Screen'),
       ),
-      body: Center(
-        child: Text('채팅 화면'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            MainChatHeader(), // 채팅화면 헤더
+            SizedBox(height: 100), 
+            MainChatRecoButtons(), // 추천 질문 버튼
+            SizedBox(height: 20),
+            MainChatSearchBar(),  // 검색바
+            SizedBox(height: 80),
+            Expanded(child: MainChatRecentView()), // 최근 조회 콘텐츠(대화) 목록
+
+
+            // 다른 UI 요소 추가예정
+          ],
+        ),
       ),
     );
   }
