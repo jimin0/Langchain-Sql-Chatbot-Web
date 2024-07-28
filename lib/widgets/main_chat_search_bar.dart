5007149665e950474595573ec9c94f5969b591b3
username <jiminkim0131@gmail.com>
2024-07-28 20:32:53 +0900

    Design: 검색창 아이콘 추가(첨부파일, 보내기아이콘)

--- lib/widgets/main_chat_search_bar.dart
+++ lib/widgets/main_chat_search_bar.dart
@@ -4,13 +4,41 @@ import 'package:flutter/material.dart';
 class MainChatSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        labelText: '메시지 SQL Chat',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0),
+    return Container(
+      height: 80.0, // 검색창의 높이를 설정
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: '메시지 SQL Chat',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          prefixIcon: GestureDetector(
+            onTap: () {
+              print("첨부파일 아이콘 클릭됨");
+            },
+            child: Icon(Icons.attach_file),
+          ),
+          suffixIcon: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                print("전송 아이콘 클릭됨");
+              },
+              child: Container(
+                width: 40.0, // 전송 버튼의 너비
+                height: 40.0, // 전송 버튼의 높이
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(Icons.send, color: Colors.white),
+              ),
+            ),
+          ),
         ),
-        suffixIcon: Icon(Icons.send),
+        onSubmitted: (String text) {
+          print("입력한 텍스트: $text");
+        },
       ),
     );
   }
