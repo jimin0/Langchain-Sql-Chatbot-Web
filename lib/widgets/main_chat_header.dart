c838c94307c999cb2ef76cbc97f9a25d0c44f62f
username <jiminkim0131@gmail.com>
2024-07-28 20:59:17 +0900

    Feat: MainChatHeader에 아이콘과 텍스트가 포함된 Refresh 버튼 추가

--- lib/widgets/main_chat_header.dart
+++ lib/widgets/main_chat_header.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart';
 
-// pages/chat_screen.dart 에 사용 - > headr 메시지
+// pages/chat_screen.dart 에 사용 - > header 메시지
 
 class MainChatHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      text: TextSpan(
-        children: [
-          TextSpan(
-            text: '옥돌민님, ',
-            style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+    return Row(
+      children: [
+        Expanded(
+          child: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: '옥돌민님, ',
+                  style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '안녕하세요. ',
+                  style: TextStyle(color: Colors.blue, fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '오늘은 어떤 도움을 드릴까요?',
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                ),
+              ],
+            ),
           ),
-          TextSpan(
-            text: '안녕하세요. ',
-            style: TextStyle(color: Colors.blue, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        OutlinedButton.icon(
+          onPressed: () {
+            print("Refresh button clikced");
+          },
+          icon: Icon(Icons.refresh),
+          label: Text('새로고침'),
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
           ),
-          TextSpan(
-            text: '오늘은 어떤 도움을 드릴까요?',
-            style: TextStyle(color: Colors.black, fontSize: 24),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
