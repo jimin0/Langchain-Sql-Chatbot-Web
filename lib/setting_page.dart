d7b0ffc1233664be41ede4ebea965f61209b9763
username <jiminkim0131@gmail.com>
2024-07-28 15:03:26 +0900

    Add: api key 입력 임시페이지

---
+++ lib/setting_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('설정'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'API Key 입력',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('뒤로 가기'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
