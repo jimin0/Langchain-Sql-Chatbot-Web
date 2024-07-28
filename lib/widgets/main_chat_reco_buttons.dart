0d26c4e14bc3b11fe9e5f77819438c5f18eccd68
username <jiminkim0131@gmail.com>
2024-07-28 16:11:52 +0900

    Refactor: MainChatRecoButtons 위젯의 버튼 좌측 정렬 및 간격 추가

--- lib/widgets/main_chat_reco_buttons.dart
+++ lib/widgets/main_chat_reco_buttons.dart
@@ -5,18 +5,20 @@ class MainChatRecoButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row( // 수평 정렬
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 동일 간격
+      mainAxisAlignment: MainAxisAlignment.end, // 동일 간격 
       children: <Widget>[
         ElevatedButton(
           onPressed: () {},
           // 임시 추천 질문 
           child: Text('매출 변동이 큰 제품은?'),
         ),
+        SizedBox(width: 10), // 버튼 간 간격
         ElevatedButton(
           onPressed: () {},
           // 임시 추천 질문 
           child: Text('오늘 매출에 대해 알려주세요.'),
         ),
+        SizedBox(width: 10), // 버튼 간 간격
         ElevatedButton(
           onPressed: () {},
           // 임시 추천 질문
