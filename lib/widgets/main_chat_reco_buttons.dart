949f033ce9675e6b4da9aee03e48dd8005910143
username <jiminkim0131@gmail.com>
2024-07-28 15:53:13 +0900

    Add: 추천버튼 위젯 추가

---
+++ lib/widgets/main_chat_reco_buttons.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row( // 수평 정렬
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 동일 간격
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () {},
+          // 임시 추천 질문 
+          child: Text('매출 변동이 큰 제품은?'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          // 임시 추천 질문 
+          child: Text('오늘 매출에 대해 알려주세요.'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          // 임시 추천 질문
+          child: Text('현재 직원 조회'),
+        ),
+      ],
+    );
+  }
+}
