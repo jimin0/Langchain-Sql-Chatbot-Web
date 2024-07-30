f8b2f666d144fa7707aec6c07e13634f8a365839
username <jiminkim0131@gmail.com>
2024-07-30 17:27:58 +0900

    Feat: 채팅 메인 화면에 채팅 bubble 추가(대화창)

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -4,9 +4,9 @@ import '../widgets/main_chat_recent_view.dart';
 import '../widgets/main_chat_reco_buttons.dart';
 import '../widgets/main_chat_search_bar.dart';
 import '../widgets/main_sidebar.dart';
+import '../components/chat_bubble_base.dart'; // 채팅 버블 위젯 임포트
 
 // 메인화면이자 채팅화면
-
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,15 +25,43 @@ class ChatScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   MainChatHeader(), // 채팅화면 헤더
-                  SizedBox(height: 100), 
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        ChatBubbleBase(
+                          message: "안녕하세용 !",
+                          isUser: true,
+                        ),
+                        ChatBubbleBase(
+                          message: "안녕하세요 ai입니다.",
+                          isUser: false,
+                        ),
+                        ChatBubbleBase(
+                          message: "제주도 가고싶어용",
+                          isUser: true,
+                        ),
+                        ChatBubbleBase(
+                          message: "제주도에 대해 알려드리겠습니다.",
+                          isUser: false,
+                        ),
+                        ChatBubbleBase(
+                          message: "너가 말한 첫번째 장소에 대해 말해봐",
+                          isUser: true,
+                        ),
+                        ChatBubbleBase(
+                          message: "한라산은~~~~~~~~~~~.",
+                          isUser: false,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
                   MainChatRecoButtons(), // 추천 질문 버튼
                   SizedBox(height: 20),
                   MainChatSearchBar(),  // 검색바
-                  SizedBox(height: 80),
+                  SizedBox(height: 20),
                   Expanded(child: MainChatRecentView()), // 최근 조회 콘텐츠(대화) 목록
-            
-            
-                  // 다른 UI 요소 추가예정
                 ],
               ),
             ),
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-
-import '../components/chat_UserChatBubble.dart';
-
 // 연결 페이지 (임시) 
 // API KEY 입력화면, Chatbot 메인화면으로 이동
 
@@ -28,15 +25,6 @@ class MainPage extends StatelessWidget {
                 Navigator.pushNamed(context, '/chat');
               },
               child: Text('Chatbot 메인 화면으로 이동'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'User Chat Bubble Example:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            UserChatBubble(
-              message: "Hello, this is a chat bubble!",
-              userName: "User",
             ),
           ],
         ),
