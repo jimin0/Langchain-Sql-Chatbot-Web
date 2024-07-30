d663bc87a53b91eaa9ff4206ef2e8c4cb4cf62c7
username <jiminkim0131@gmail.com>
2024-07-30 17:43:14 +0900

    Fix: hover 안되는 문제 수정

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import '../components/chat_aichatbubble.dart';
+import '../components/chat_userchatbubble.dart';
 import '../widgets/main_chat_header.dart';
 import '../widgets/main_chat_recent_view.dart';
 import '../widgets/main_chat_reco_buttons.dart';
 import '../widgets/main_chat_search_bar.dart';
 import '../widgets/main_sidebar.dart';
-import '../components/chat_bubble_base.dart'; // 채팅 버블 위젯 임포트
 
 // 메인화면이자 채팅화면
 class ChatScreen extends StatelessWidget {
@@ -16,52 +17,49 @@ class ChatScreen extends StatelessWidget {
       ),
       backgroundColor: Colors.white,
       body: Row(
-        children: <Widget>[
-          MainSidebar(), // 사이드바
+        children: [
+          MainSidebar(),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  MainChatHeader(), // 채팅화면 헤더
+                children: [
+                  MainChatHeader(),
                   SizedBox(height: 20),
                   Expanded(
                     child: ListView(
                       children: [
-                        ChatBubbleBase(
+                        UserChatBubble(
                           message: "안녕하세용 !",
-                          isUser: true,
+                          userName: "User",
                         ),
-                        ChatBubbleBase(
+                        AIChatBubble(
                           message: "안녕하세요 ai입니다.",
-                          isUser: false,
                         ),
-                        ChatBubbleBase(
+                        UserChatBubble(
                           message: "제주도 가고싶어용",
-                          isUser: true,
+                          userName: "User",
                         ),
-                        ChatBubbleBase(
+                        AIChatBubble(
                           message: "제주도에 대해 알려드리겠습니다.",
-                          isUser: false,
                         ),
-                        ChatBubbleBase(
+                        UserChatBubble(
                           message: "너가 말한 첫번째 장소에 대해 말해봐",
-                          isUser: true,
+                          userName: "User",
                         ),
-                        ChatBubbleBase(
+                        AIChatBubble(
                           message: "한라산은~~~~~~~~~~~.",
-                          isUser: false,
                         ),
                       ],
                     ),
                   ),
                   SizedBox(height: 20),
-                  MainChatRecoButtons(), // 추천 질문 버튼
+                  MainChatRecoButtons(),
                   SizedBox(height: 20),
-                  MainChatSearchBar(),  // 검색바
+                  MainChatSearchBar(),
                   SizedBox(height: 20),
-                  Expanded(child: MainChatRecentView()), // 최근 조회 콘텐츠(대화) 목록
+                  Expanded(child: MainChatRecentView()),
                 ],
               ),
             ),
